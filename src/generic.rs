43b8067057712ff74fe4c2f05962d48ccf756ab4
artimonist <artimonist@gmail.com>
2025-02-03 00:38:31 +0800

    new impl of simple diagram and complex diagram

--- src/generic.rs
+++ src/generic.rs
@@ -58,38 +58,37 @@ type Matrix<const H: usize, const W: usize, T> = [[Option<T>; W]; H];
 
 impl<const H: usize, const W: usize, T: Serialize> GenericDiagram<H, W, T> for Matrix<H, W, T> {
     fn to_secret(&self) -> GenericResult<Vec<u8>> {
-        let indices = self
-            .iter()
-            .flat_map(|r| r.iter())
-            .map(|v| v.is_some())
-            .to_bits();
-        let items: Vec<_> = self
-            .into_iter()
-            .flat_map(|r| r.into_iter())
-            .flatten()
-            .collect();
-        let vs = rmp_serde::to_vec(&(indices, items))?;
-        Ok(vs)
+        let mut items = Vec::new();
+        let mut indices = Vec::with_capacity(H * W);
+
+        (0..W).rev().for_each(|col| {
+            (0..H).rev().for_each(|row| {
+                if let Some(v) = &self[row][col] {
+                    items.push(v);
+                    indices.push(true);
+                } else {
+                    indices.push(false);
+                }
+            });
+        });
+
+        let indices = indices.into_iter().to_bits();
+        let data = rmp_serde::to_vec(&(indices, items))?;
+        Ok(data)
     }
 }
 
 /// transform vector to generic diagram
-pub trait VecDiagram<const H: usize, const W: usize, T: Serialize> {
-    /// transform vector to generic diagram
-    fn to_diagram(self) -> Matrix<H, W, T>;
+pub trait VecDiagram<T: Serialize> {
+    /// transform vector to matrix
+    fn to_matrix<const H: usize, const W: usize>(self) -> Matrix<H, W, T>;
 }
 
-impl<const H: usize, const W: usize, T: Serialize + Copy> VecDiagram<H, W, T> for Vec<Option<T>> {
-    fn to_diagram(mut self) -> Matrix<H, W, T> {
-        // if not enough items, padding None values
-        {
-            let len = match self.len() < H * W {
-                true => H * W - self.len(),
-                false => 0,
-            };
-            self.append(&mut vec![None; len]);
-        };
-        core::array::from_fn(|row| core::array::from_fn(|col| self[row * W + col]))
+impl<T: Serialize> VecDiagram<T> for Vec<Option<T>> {
+    fn to_matrix<const H: usize, const W: usize>(mut self) -> Matrix<H, W, T> {
+        self.resize_with(H * W, || None);
+        self.reverse();
+        core::array::from_fn(|_| core::array::from_fn(|_| self.pop().unwrap()))
     }
 }
 
@@ -130,21 +129,39 @@ mod generic_test {
             None,
             Some(0),
         ];
-        const ENTROPY: &str = "5cb583949c32a6bcfc8252e7b0a0ca5663ded466cb1ead68f539ba784f8f6da3";
-        const XPRIV: &str = "xprv9s21ZrQH143K2HQAyrRqc9HvvxEBsehBm2V2XdxFLqrATdboEjatXbSmYtPSBxKRtrTAqQQ4NZ93rnjBoDvw5poWpH2Tfxm27M6imD8doLn";
+        const ENTROPY: &str = "726ea65196a104c64f845d93792fba3a149aaa8b1af323e3d474394bf7c204b2";
+        const XPRIV: &str = "xprv9s21ZrQH143K26wqw5cyn4qGD2CsyVH2Lpma622cgETpFvNfnPAGpmkFisKjr3G3SUKoCXXkctNssYpAXuVeZBw2HmihXxnwYUxicZM2Spt";
         {
+            // MATRIX easy to use
             let entropy = MATRIX.to_entropy("test".as_bytes())?;
             let master = MATRIX.to_master("test".as_bytes())?;
             assert_eq!(entropy.to_lower_hex_string(), ENTROPY);
             assert_eq!(master.to_string(), XPRIV);
         }
         {
-            let matrix: Matrix<3, 5, u128> = VECTOR.to_vec().to_diagram();
-            let entropy = matrix.to_entropy("test".as_bytes())?;
-            let master = matrix.to_master("test".as_bytes())?;
-            assert_eq!(entropy.to_lower_hex_string(), ENTROPY);
+            // VECTOR equal to MATRIX
+            let vector = VECTOR.to_vec();
+            let master = vector.to_matrix::<3, 5>().to_master("test".as_bytes())?;
             assert_eq!(master.to_string(), XPRIV);
+        }
+        {
+            // verify vector to matrix sequence
+            const MATRIX: [[Option<u8>; 3]; 3] = [
+                [Some(1), Some(2), Some(3)],
+                [Some(4), Some(5), Some(6)],
+                [Some(7), Some(8), Some(9)],
+            ];
+            let vector: Vec<Option<u8>> =
+                [1, 2, 3, 4, 5, 6, 7, 8, 9].into_iter().map(Some).collect();
+            let matrix = vector.to_matrix::<3, 3>();
+            assert_eq!(matrix, MATRIX);
         }
         Ok(())
     }
 }
+
+/// Simple Diagram
+pub type GenericSimple = Matrix<7, 7, char>;
+
+/// Complex Diagram
+pub type GenericComplex = Matrix<7, 7, String>;
